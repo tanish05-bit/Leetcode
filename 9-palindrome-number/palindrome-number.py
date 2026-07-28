fc4dcba4e4322351cfbca40a23d7223138af5337
Tanish Sharma <tanish052008@gmail.com>
2026-07-29 00:20:36 +0530

    Time: 12 ms (49.08%) | Memory: 12.4 MB (17.77%) - LeetSync

--- 9-palindrome-number/palindrome-number.py
+++ 9-palindrome-number/palindrome-number.py
@@ -1,8 +1,7 @@
 class Solution(object):
     def isPalindrome(self, x):
-        L=list(str(x))
-        reverse=L[::-1]
-        return L==reverse
+        x=list(str(x))
+        return x==x[::-1]
         
 
 
