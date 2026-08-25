2eab059ffdf616c5b61636b1cb6212f6195aa98e
Tanish Sharma <tanish052008@gmail.com>
2026-08-25 11:57:09 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (50.57%) - LeetSync

--- 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
+++ 4080-smallest-missing-multiple-of-k/smallest-missing-multiple-of-k.py
@@ -1,18 +1,12 @@
 class Solution(object):
     def missingMultiple(self, nums, k):
+        i=True
         n=1
-        m=True
-        L=[]
-        for i in nums:
-            if i%k==0:
-                L.append(i)
-            else:
-                continue
-        L.sort()
-        while m is True:
-            if k*n in L:
+        while i is True:
+            if k*n in nums:
                 n+=1
             else:
                 return(k*n)
-                k=False
+                i=False
+
 
