4ba25e0ffa7ed72019f98679c77e85f97778255b
Tanish Sharma <tanish052008@gmail.com>
2026-09-16 15:35:17 +0530

    Time: 0 ms (100.00%) | Memory: 12.4 MB (18.23%) - LeetSync

---
+++ 27-remove-element/remove-element.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def removeElement(self, nums, val):
+        i=0
+        for k in nums:
+            if k!=val:
+                nums[i]=k
+                i+=1
+        return(i)
+
+
+        
