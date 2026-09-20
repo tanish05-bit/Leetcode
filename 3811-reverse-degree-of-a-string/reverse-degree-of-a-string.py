09818618f5161c4e0555f755edfad02b6b4ec831
Tanish Sharma <tanish052008@gmail.com>
2026-09-20 15:56:20 +0530

    Time: 7 ms (91.14%) | Memory: 12.3 MB (89.67%) - LeetSync

--- 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
+++ 3811-reverse-degree-of-a-string/reverse-degree-of-a-string.py
@@ -2,7 +2,6 @@ class Solution(object):
     def reverseDegree(self, s):
         a=0
         b=1
-        for i in s:
-            a+=(27-(ord(i)-96))*b
-            b+=1
-        return(a)
+        for i in range(len(s)):
+            a+=(123-ord(s[i]))*(i+1)
+        return (a)
